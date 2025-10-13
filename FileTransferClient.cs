e5377a98a81f587fd7e7257e730596d84cc6b626
thewhobox <45664417+thewhobox@users.noreply.github.com>
2025-10-13 09:00:30 +0200

    removed: printinfo in fileinfo

--- FileTransferClient.cs
+++ FileTransferClient.cs
@@ -186,7 +186,6 @@ public class FileTransferClient
         {
             int size = BitConverter.ToInt32(res.Data.Skip(1).Take(4).Reverse().ToArray(), 0);
             FileInfo info = new FileInfo(size, res.Data.Skip(5).Take(4).ToArray());
-            PrintInfo?.Invoke($"File: {path} - Size: {size} bytes - CRC32: {info.GetCrc()}");
             return info;
         }
         else
